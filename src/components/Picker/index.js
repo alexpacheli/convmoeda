97a08df7032c65a936088cec34ca9a143a8a0fa8
alexpacheli <alex@pacheli.com.br>
2022-11-26 10:57:33 -0300

    Conversor de Moedas para BRL

---
+++ src/components/Picker/index.js
@@ -0,0 +1,33 @@
+import React from "react";
+import RNPickerSelect from 'react-native-picker-select';
+
+export default function Picker(props) {
+
+    const placeholder = {
+        label: 'Selecione sua moeda',
+        value: null,
+        color: '#000'
+    }
+
+    return (
+        <RNPickerSelect 
+            items={props.moedas}
+            onValueChange={(valor) => props.onChange(valor)}
+            placeholder={placeholder}
+            style={{
+                inputIOS: {
+                    fontSize: 20,
+                    color: '#000'
+                },
+                inputAndroid: {
+                    fontSize: 20,
+                    color: '#000'
+                }
+            }}
+        />
+    );
+}
+
+/*
+npm i react-native-picker-select --legacy-peer-deps
+*/
